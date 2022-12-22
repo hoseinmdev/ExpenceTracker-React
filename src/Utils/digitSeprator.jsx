d190abe3e3ed0bbbdd5e12cb92605ab4cccc00c6
“HoseinMahmoudi” <Hoseindev@gmail.com>
2022-12-22 19:56:30 +0330

    feat: implement Utils folder

---
+++ src/Utils/digitSeprator.jsx
@@ -0,0 +1,7 @@
+const digitSeprator = (number) => {
+  const separatedNumber = number.toLocaleString("fa");
+  // console.log(separatedNumber)
+  return separatedNumber;
+};
+
+export default digitSeprator;
